2491da64e858dcec9c7815132fd5251884cd84d1
Wonbin Lee <lob3767@gmail.com>
2022-06-07 20:11:26 +0900

    [Create] DB models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
-# Create your models here.
+from users.models           import User
+from characteristics.models import Answer, Stack
+from utils.timestamp        import TimestampZone
+
+
+class Category(TimestampZone):
+    title = models.CharField(max_length=40)
+    
+    class Meta:
+        db_table = 'categories'
+
+class Post(TimestampZone):
+    user            = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    category        = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='posts')
+    title           = models.CharField(max_length=200)
+    number_of_front = models.IntegerField()
+    number_of_back  = models.IntegerField()
+    period          = models.CharField(max_length=50)
+    description     = models.TextField(blank=True, null=True)
+    start_date      = models.DateTimeField()
+    
+    class Meta:
+        db_table = 'posts'
+        
+class ApplyWay(models.Model):
+    title = models.CharField(max_length=200)
+    
+    class Meta:
+        db_table = 'applyways' 
+
+class PostAnswer(TimestampZone): 
+    post       = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='postanswers')
+    answer     = models.ForeignKey(Answer, on_delete=models.CASCADE, related_name='postanswers')
+    is_primary = models.BooleanField(default=False)
+    
+    class Meta:
+        db_table = 'postanswers'
+
+class PostStack(models.Model):
+    post  = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='poststacks')
+    stack = models.ForeignKey(Stack, on_delete=models.CASCADE, related_name='poststacks')
+    
+    class Meta:
+        db_table = 'poststacks'
+        
+class PostApplyWay(models.Model): 
+    post     = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='postapplyways')
+    applyway = models.ForeignKey(ApplyWay, on_delete=models.CASCADE, related_name='postapplyways')
+    
+    class Meta:
+        db_table = 'postapplyways'
--- wesalad/settings.py
+++ wesalad/settings.py
@@ -24,8 +24,8 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
+    # 'django.contrib.admin',
+    # 'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
@@ -33,14 +33,15 @@ INSTALLED_APPS = [
     'corsheaders',
     'users',
     'posts',
+    'characteristics',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
@@ -95,13 +96,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
