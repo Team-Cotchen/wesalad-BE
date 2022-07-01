8bca7c6ba199cf47da289a32c08146888ad0285c
angrybirdpark <thesmileless90@gmail.com>
2022-07-01 17:24:20 +0900

    Modify : 효정님 미팅 전
    Modify : Post 1st

--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,47 @@
-from rest_framework.views    import APIView
-from rest_framework.response import Response
+from rest_framework.views       import APIView
+from rest_framework.response    import Response
+from rest_framework.permissions import IsAuthenticated
 
 from .models     import Post
-from .serializer import PostSerializer
+from .serializer import PostCreateSerializer, PostDetailSerializer, PostSimpleSerializer
 
-class PostListView(APIView):
+class PostSimpleView(APIView):
     def get(self, request):
-        posts      = Post.objects.filter()
-        serializer = PostSerializer(posts, many=True)
-        
+        posts      = Post.objects.all()
+        serializer = PostSimpleSerializer(posts, many=True)
         return Response(serializer.data, status=200)
+    
+class PostDetailView(APIView):
+    def get(self, request, pk):
+        try:
+            post       = Post.objects.get(id = pk)
+            serializer = PostDetailSerializer(post)
+            return Response(serializer.data, status=200)
+        except Post.DoesNotExist:
+            return Response("Post does not exist", status=400)
+
+class PostCreateView(APIView):
+    # permission_classes = [IsAuthenticated]
+    
+    def post(self, request):
+        category      = request.data.get("category")
+        answers       = request.data.get("answers")
+        stacks        = request.data.get("stacks")
+        applyway      = request.data.get("applyway")
+        applyway_info = request.data.get("applyway_info")
+        place         = request.data.get("place")
+        flavor        = request.data.get("flavor")
+        
+        serializer = PostCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(
+                category      = category,
+                answers       = answers,
+                stacks        = stacks,
+                applyway      = applyway,
+                applyway_info = applyway_info,
+                place         = place,
+                flavor        = flavor
+            )
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
