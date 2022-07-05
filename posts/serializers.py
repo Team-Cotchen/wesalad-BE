b2fdae5a79b52b5a502c8f87941e179522a23e77
angrybirdpark <thesmileless90@gmail.com>
2022-07-05 18:30:42 +0900

    Modify : Pagination

--- posts/serializers.py
+++ posts/serializers.py
@@ -2,10 +2,12 @@ import json
 
 from rest_framework import serializers
 
-from django.db import transaction
+from django.db              import transaction
+from django.core.exceptions import ObjectDoesNotExist
 
 from characteristics.models import Answer, Stack
 from users.models           import GoogleSocialAccount, User
+from utils.utils            import error_message
 from .models                import (ApplyWay, Category, Flavor, Place,
                                     Post, PostAnswer, PostApplyWay, PostFlavor,
                                     PostPlace, PostStack)
@@ -116,12 +118,12 @@ class PostFlavorSerializer(serializers.ModelSerializer):
         model  = PostFlavor
         fields = ['title', 'description', 'image_url']
 
-class PostSimpleSerializer(serializers.ModelSerializer):
+class PostSerializer(serializers.ModelSerializer):
     category      = serializers.SerializerMethodField()
     post_answer   = serializers.SerializerMethodField()
-    post_stack    = PostStackSerializer(source='poststacks', many=True)
-    post_applyway = PostApplyWaySerializer(source='postapplyways', many=True)
-    post_flavor   = PostFlavorSerializer(source='postflavors', many=True)
+    post_stack    = PostStackSerializer(source='poststacks', many=True, required=False)
+    post_applyway = PostApplyWaySerializer(source='postapplyways', many=True, required=False)
+    post_flavor   = PostFlavorSerializer(source='postflavors', many=True, required=False)
     post_place    = serializers.SerializerMethodField()
     user          = UserSerializer(required=False)
     
@@ -148,83 +150,47 @@ class PostSimpleSerializer(serializers.ModelSerializer):
         postplace = post.postplaces.get(post = post)
         return postplace.place.title
     
-    class Meta:
-        model  = Post
-        fields = '__all__'
-
-class PostDetailSerializer(serializers.ModelSerializer):
-    category      = serializers.SerializerMethodField()
-    post_answer   = PostAnswerSerializer(source='postanswers', many=True)
-    post_stack    = PostStackSerializer(source='poststacks', many=True)
-    post_applyway = PostApplyWaySerializer(source='postapplyways', many=True)
-    post_flavor   = PostFlavorSerializer(source='postflavors', many=True)
-    post_place    = serializers.SerializerMethodField()
-    user          = UserSerializer(required=False)
-    
-    def get_category(self, post):
-        return post.category.title
-    
-    def get_post_place(self, post):
-        postplace = post.postplaces.get(post = post)
-        return postplace.place.title
-    
-    class Meta:
-        model  = Post
-        fields = '__all__'
-        
-class PostCreateSerializer(serializers.ModelSerializer):
-    category      = serializers.SerializerMethodField()
-    post_answer   = PostAnswerSerializer(source='postanswers', many=True, required=False)
-    post_stack    = PostStackSerializer(source='poststacks', many=True, required=False)
-    post_applyway = PostApplyWaySerializer(source='postapplyways', many=True, required=False)
-    post_flavor   = PostFlavorSerializer(source='postflavors', many=True, required=False)
-    post_place    = serializers.SerializerMethodField()
-    user          = UserSerializer(required=False)
-    
-    def get_category(self, post):
-        return post.category.title
-    
-    def get_post_place(self, post):
-        postplace = post.postplaces.get(post = post)
-        return postplace.place.title
-    
     @transaction.atomic()
     def create(self, validated_data):
-        category      = validated_data.pop("category", None)
-        answers       = validated_data.pop("answers", None)
-        stacks        = validated_data.pop("stacks", None)
-        applyway      = validated_data.pop("applyway", None)
-        applyway_info = validated_data.pop("applyway_info", None)
-        place         = validated_data.pop("place", None)
-        flavor        = validated_data.pop("flavor", None)
-        
-        user_instance     = User.objects.get(id = 1)
-        category_instance = Category.objects.get(title = category)
-        applyway_instance = ApplyWay.objects.get(title = applyway)
-        place_instance    = Place.objects.get(title = place)
-        flavor_instance   = Flavor.objects.get(title = flavor)
-        
-        stacks  = json.loads(stacks)
-        answers = json.loads(answers)
-        
-        post = Post.objects.create(
-            user     = user_instance,
-            category = category_instance,
-            **validated_data
-            )
-        if not post:
-            raise serializers.ValidationError("post isn't create")
-        
-        post.postplaces.create(place = place_instance)
-        post.postflavors.create(flavor = flavor_instance)
-        post.postapplyways.create(applyway = applyway_instance, description = applyway_info)
-        [post.poststacks.create(stack = Stack.objects.get(title = stack)) for stack in stacks]
-        [post.postanswers.create(
-            answer     = Answer.objects.get(description = answer.get("description")),
-            is_primary = answer.get("is_primary")
-            ) for answer in answers]
+        try:
+            category      = validated_data.pop("category", None)
+            answers       = validated_data.pop("answers", None)
+            stacks        = validated_data.pop("stacks", None)
+            applyway      = validated_data.pop("applyway", None)
+            applyway_info = validated_data.pop("applyway_info", None)
+            place         = validated_data.pop("place", None)
+            flavor        = validated_data.pop("flavor", None)
+            
+            user_instance     = User.objects.get(id = 1)
+            category_instance = Category.objects.get(title = category)
+            applyway_instance = ApplyWay.objects.get(title = applyway)
+            place_instance    = Place.objects.get(title = place)
+            flavor_instance   = Flavor.objects.get(title = flavor)
+            
+            stacks  = json.loads(stacks)
+            answers = json.loads(answers)
+            
+            post = Post.objects.create(
+                user     = user_instance,
+                category = category_instance,
+                **validated_data
+                )
+            if not post:
+                raise serializers.ValidationError("post isn't create")
+            
+            post.postplaces.create(place = place_instance)
+            post.postflavors.create(flavor = flavor_instance)
+            post.postapplyways.create(applyway = applyway_instance, description = applyway_info)
+            [post.poststacks.create(stack = Stack.objects.get(title = stack)) for stack in stacks]
+            [post.postanswers.create(
+                answer     = Answer.objects.get(description = answer.get("description")),
+                is_primary = answer.get("is_primary")
+                ) for answer in answers]
 
-        return post
+            return post
+        except ObjectDoesNotExist as e:
+            raise serializers.ValidationError(error_message(f'{e}')) 
+            
     
     class Meta:
         model  = Post
--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from posts.views import PostCreateView, PostDetailView, PostSimpleView
+from posts.views import PostCreateView, PostDetailView, PostListView
 
 urlpatterns = [
-    path('', PostSimpleView.as_view()),
+    path('', PostListView.as_view()),
     path('/<int:pk>', PostDetailView.as_view()),
     path('/create', PostCreateView.as_view()),
 ]
